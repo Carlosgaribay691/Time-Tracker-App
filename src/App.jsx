3a1947193fbaab08ba3240fce2c2099d5d1b2792
Carlos Garibay <carlosgaribay691@outlook.es>
2025-07-13 16:23:44 -0600

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import TimeLog from './pages/TimeLog_Temp'
+import TimeLog from './pages/Temp'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/timelog" element={<TimeLog />} />
+      <Route path="/timelog" element={<Temp />} />
     </Routes>
   );
 }
